3d8e4019bf1ea4cfd0ebf9c30f37c42f895309ea
Mark <mtdoss@gmail.com>
2015-02-12 11:15:36 -0800

    Heapify down passing all specs

--- lib/heap.rb
+++ lib/heap.rb
@@ -27,6 +27,31 @@ class Heap
     end
 
     arr[child_idx], arr[parent_idx] = parent_val, child_val
-    heapify_up(arr, parent_idx, len)
+    heapify_up(arr, parent_idx)
+  end
+
+  def self.heapify_down(arr, parent_idx, len = arr.length)
+    return arr if parent_idx == len - 1
+    left_child_index, right_child_index = child_indices(parent_idx, len)
+
+    parent_val = arr[parent_idx]
+
+    children = []
+    children << arr[left_child_index] if left_child_index
+    children << arr[right_child_index] if right_child_index
+    return arr if children.empty?
+
+    if children.all? { |el| parent_val < el }
+      return arr
+    end
+    swap_idx = nil
+    if children.length == 1
+      swap_idx = left_child_index
+    else
+      swap_idx = children[0] < children[1] ? left_child_index : right_child_index
+    end
+
+    arr[parent_idx], arr[swap_idx] = arr[swap_idx], arr[parent_idx]
+    heapify_down(arr, swap_idx)
   end
 end
