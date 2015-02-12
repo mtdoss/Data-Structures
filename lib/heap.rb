14ad6765734481d8ec7e09bf9c7161dac6839bff
Mark <mtdoss@gmail.com>
2015-02-12 10:56:36 -0800

    Heapify up working; have to remember to return if child_idx = 0

--- lib/heap.rb
+++ lib/heap.rb
@@ -17,6 +17,16 @@ class Heap
   end
 
   def self.heapify_up(arr, child_idx, len = arr.length)
-    
+    return arr if child_idx == 0
+    child_val = arr[child_idx]
+    parent_idx = parent_index(child_idx, len)
+    parent_val = arr[parent_idx]
+
+    if child_val >= parent_val
+      return arr
+    end
+
+    arr[child_idx], arr[parent_idx] = parent_val, child_val
+    heapify_up(arr, parent_idx, len)
   end
 end
