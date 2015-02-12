2afa8e0fab7f700ca45510f485e58d42cb6ae60b
Mark <mtdoss@gmail.com>
2015-02-12 10:44:52 -0800

    child, parent indices working

--- lib/heap.rb
+++ lib/heap.rb
@@ -1,6 +1,22 @@
 # Binary Min Heap
 class Heap
   def initialize
+    self.store = []    
+  end
+
+  def count
+    store.length
+  end
+
+  def self.child_indices(parent_index, len)
+    [(parent_index * 2) + 1, (parent_index * 2) + 2].select { |i| i < len } 
+  end
+
+  def self.parent_index(child_index, len)
+    (child_index - 1) / 2    
+  end
+
+  def self.heapify_up(arr, child_idx, len = arr.length)
     
   end
 end
