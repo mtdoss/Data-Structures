9414feb6ce885ac53779481c4ec54258c98c75bf
Mark <mtdoss@gmail.com>
2015-02-11 18:15:21 -0800

    More specs

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,8 +2,8 @@ require 'byebug'
 require 'linked_list'
 
 describe LinkedList do
+  subject(:list) { LinkedList.new }
   it "pushes links into the list" do
-    list = LinkedList.new
     link1 = Link.new(5)
     link2 = Link.new(10)
     list.push_link(link1)
@@ -19,4 +19,12 @@ describe LinkedList do
     expect(link1.prev).to eq(list.first)
     expect(list.first.next).to eq(link1)
   end
+
+  it "pushes values into the list" do
+    list.push(5)
+    list.push(10)
+    expect(list[0].value).to eq(5)
+    expect(list[1].value).to eq(10)
+  end
+
 end
