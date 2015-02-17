2cbff63f80becd0117e8f6491bc5d56cbc24217d
Mark <mtdoss@gmail.com>
2015-02-17 09:57:08 -0800

    Added some BST functionality

--- lib/unbalanced_bst.rb
+++ lib/unbalanced_bst.rb
@@ -1,4 +1,5 @@
 class Node
+  attr_accessor :value, :left, :right
   def initialize(val)
     @value = val
     @left = nil
@@ -16,8 +17,43 @@ class BST
     if @root.nil?
       @root = node
     else
-      if true
-      end
+      insert_at_node(@root, node)
     end
   end
+
+  def insert_at_node(root_node, new_node)
+    root_val, new_val = root_node.value, new_node.value
+    if new_val <= root_val
+      if root_node.left
+        insert_at_node(root_node.left, new_node)
+      else
+        root_node.left = new_node
+      end 
+    else
+      if root_node.right
+        insert_at_node(root_node.right, new_node)
+      else
+        root_node.right = new_node
+      end 
+    end
+  end
+
+  def in_order(node)
+    return if node.nil?
+    in_order(node.left)
+    p node.value
+    in_order(node.right)
+  end
 end
+
+
+test = BST.new
+test.insert(5)
+test.insert(4)
+test.insert(7)
+test.insert(15)
+test.insert(1)
+test.insert(2)
+test.insert(8)
+test.insert(3)
+in_order(test.root)
