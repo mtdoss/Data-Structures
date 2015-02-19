3ef04e7276b68ac611cc20e3f181266cc36bf951
Mark <mtdoss@gmail.com>
2015-02-19 11:07:27 -0800

    Bracket matcher almost done

--- js-code/bracket-matcher.js
+++ js-code/bracket-matcher.js
@@ -1,3 +1,22 @@
 function bracketMatcher(str) {
-  
+  var stack = [];
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charAt(i);
+    if (char === "[" || char === "(") {
+      stack.push(str.charAt(i));
+    }
+    if (char === "]" && stack.pop !== "[") {
+      return false;
+    }
+    if (char === ")" && stack.pop !== "(") {
+      return false;
+    }
+  }  
+
+  if (stack.length === 0) {
+    return true;
+  }
 }
+
+console.log(bracketMatcher("[]()()"));
+console.log(bracketMatcher("[(])"));
