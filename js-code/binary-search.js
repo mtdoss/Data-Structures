b12a564144b28cd95da3823bc3b969a4c5df027c
Mark <mtdoss@gmail.com>
2015-02-19 13:09:45 -0800

    fixed bug in binary search

--- js-code/binary-search.js
+++ js-code/binary-search.js
@@ -1,17 +1,22 @@
 function binarySearch(arr, target) {
   var len = arr.length;
   var mid = Math.floor(len / 2);  
-  console.log(len);
-  console.log(mid);
 
-  var leftArray = arr.slice(0, mid + 1);
-  var rightArray = arr.slice(mid + 1, len);
+  var leftArray = arr.slice(0, mid);
+  var rightArray = arr.slice(mid, len);
   if (arr[mid] === target) {
     return mid;
+    
   } else if (arr[mid] > target) {
+    // console.log(arr[mid]);
+    // console.log(mid);
+    // console.log(arr);
     return binarySearch(leftArray, target);
-  } else {
+  } else if (arr[mid] < target) {
     return mid + binarySearch(rightArray, target);
+  } else {
+    console.log("error!!");
+    return "ERROR IT'S NOT HERE";
   }
 }
 
