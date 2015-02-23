f75566a16c3ba29f727042aa0f4b4aa0403874e4
Mark <mtdoss@gmail.com>
2015-02-22 16:26:24 -0800

    Oops, there's a bug with partition

--- spec/quick_sort_spec.rb
+++ spec/quick_sort_spec.rb
@@ -4,7 +4,7 @@ describe QuickSort do
  describe "partition" do
   it 'partitions the whole array properly' do
     arr = [3, 5, 6, 1, 9]
-    pivot_idx = Quicksort.partition(arr, 0, 5) 
+    pivot_idx = QuickSort.partition(arr, 0, 5) 
     expect(arr).to eq([1, 3, 6, 5, 9])
     expect(pivot_idx).to eq(1)
   end 
@@ -13,8 +13,14 @@ describe QuickSort do
  describe "my_qs!" do
    it 'sorts an array with no duplicates' do
      arr = [3, 5, 6, 1, 2]
-     Quicksort.my_qs!(arr)
+     QuickSort.my_qs!(arr)
      expect(arr).to eq([1, 2, 3, 5, 6])
+   end
+
+   it 'sorts an array with duplicates' do
+     arr = [3, 4, 1, 2, 3, 6, 7, 2, 1]
+     QuickSort.my_qs!(arr)
+     expect(arr).to eq([1, 1, 2, 2, 3, 3, 4, 6, 7])
    end
 
    it 'makes the right number of comparisons'
