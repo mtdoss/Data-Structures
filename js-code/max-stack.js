77cd0d12b0b6533992a5dd0436506ee7aaf853f8
Mark <mtdoss@gmail.com>
2015-02-25 14:27:16 -0800

    finished minmaxstack for js, not tested

--- js-code/max-stack.js
+++ js-code/max-stack.js
@@ -5,11 +5,30 @@ function MinMaxStack() {
 
 MinMaxStack.prototype.push = function(val) {
   if (this.values.length === 0) {
-    values.push( {
+    this.values.push( {
       value: val,
       max: val,
       min: val
     });
+  } else {
+    newMax = Math.max(val, this.max);
+    newMin = Math.min(val, this.min);
+    this.values.push( {
+      value: val,
+      max: newMax,
+      min: newMin
+    });
   }
 };
-  
+
+MinMaxStack.prototype.pop = function (val) {
+  return this.values.pop.value;
+};
+
+MinMaxStack.prototype.max = function () {
+  return this.values.length === 0 ? null : this.values[this.values.length - 1].max;
+};
+
+MinMaxStack.prototype.min = function () {
+  return this.values.length === 0 ? null : this.values[this.values.length - 1].min;
+};
