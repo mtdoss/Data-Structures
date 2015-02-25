85ce6e592e6f4a51c7b38aa09365ac884e04b1ce
Mark <mtdoss@gmail.com>
2015-02-25 14:20:32 -0800

    working on max stack javascript

---
+++ js-code/max-stack.js
@@ -0,0 +1,15 @@
+function MinMaxStack() {
+  this.values = [];
+}
+
+
+MinMaxStack.prototype.push = function(val) {
+  if (this.values.length === 0) {
+    values.push( {
+      value: val,
+      max: val,
+      min: val
+    });
+  }
+};
+  
